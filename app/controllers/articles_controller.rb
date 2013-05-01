51f14b2c34480c905fadcd35eb674bcb7243a727
Robert Herman <robert.herman@gettyimages.com>
2013-05-01 10:14:17 -0600

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,44 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+  
   def show
-   @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    flash.notice = "Article '#{@article.title}' Updated!"
+    # 2 other ways to accomplish this flash notice
+    #flash[:notice] = "Article '#{@article.title}' Updated!"
+    #redirect_to article_path(@article), :notice => "Test Flash"
+    redirect_to article_path(@article)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
+    # 2 other Created!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    # 2 other Deleted!"
+    redirect_to articles_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
+
   #this tells Rails that we have a resource named articles and router should
   #expect RESTful interation to the resource
   resources :articles
