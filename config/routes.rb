5a62e343cd5ceb4dd54c749a384c09a9cbd0e98d
Robert Herman <robert.herman@gettyimages.com>
2013-05-01 14:34:30 -0600

    Finished adding blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Blogger::Application.routes.draw do
 
   #this tells Rails that we have a resource named articles and router should
   #expect RESTful interation to the resource
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
