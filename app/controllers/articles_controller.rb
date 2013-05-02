672f725081fb6a921e94e79f0ac8fc9beaf07a49
Robert Herman <robert.herman@gettyimages.com>
2013-05-02 13:33:43 -0600

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,9 +18,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update_attributes(params[:article])
     flash.notice = "Article '#{@article.title}' Updated!"
-    # 2 other ways to accomplish this flash notice
-    #flash[:notice] = "Article '#{@article.title}' Updated!"
-    #redirect_to article_path(@article), :notice => "Test Flash"
     redirect_to article_path(@article)
   end
 
@@ -31,16 +28,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
     @article.save
-    flash.notice = "Article '#{@article.title}' Created!"
-    # 2 other Created!"
+    flash.notice = "Article '#{ @article.title }' Created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash.notice = "Article '#{@article.title}' Deleted!"
-    # 2 other Deleted!"
+    flash.notice = "Article '#{ @article.title }' Deleted!"
     redirect_to articles_path
   end
 end
