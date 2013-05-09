93bcb5ca536fe053aadffbda49367357541ca0f9
Robert Herman <robert.herman@gettyimages.com>
2013-05-09 07:16:43 -0600

    added some model unit testing

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
-  # attr_accessible :email, :password
+  validates :username, :presence => true, :uniqueness => true
+  # attr_accessible :username, :password
 end
