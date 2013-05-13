fd2b16687d7efe241456d3a87e508c4a4049bf87
Robert Herman <robert.herman@gettyimages.com>
2013-05-13 11:29:15 -0600

    Added author has many articles relationship to the author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,5 @@ class Author < ActiveRecord::Base
   validates_confirmation_of :password, message: "should match confirmation", if: :password
   validates :username, :presence => true, :uniqueness => true
   # attr_accessible :username, :password
+  has_many :articles
 end
